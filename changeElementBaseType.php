3c1445e250809049f36ad59429bc4fba44a64159
arbuzovan <arbuzovan@yandex.ru>
2017-08-30 16:54:47 +0300

    заданы ID. Итоговый вариант

--- changeElementBaseType.php
+++ changeElementBaseType.php
@@ -17,13 +17,36 @@
     echo '$objectTypeId = '.$objectTypeId.'<br>';
     
     $hierchyElemnetIdsToChange = array();   // Массив ID для изменения
-    $hierchyElemnetIdsToChange[] = 57;
+    
+    $hierchyElemnetIdsToChange[] = 57;  // Мальчики -> головные уборы
+    
+    $hierchyElemnetIdsToChange[] = 60;  // Сумки и рюкзаки -> Детсские рюкзаки
+    $hierchyElemnetIdsToChange[] = 61;  //  Сумки и рюкзаки -> Сумки для мамы
+    
+    $hierchyElemnetIdsToChange[] = 66;  //  Домашняя коллекция -> Игровые коврики
+    $hierchyElemnetIdsToChange[] = 67;  //  Домашняя коллекция -> Корзины для игрушек
+    $hierchyElemnetIdsToChange[] = 68;  //  Домашняя коллекция -> Пледы
+    $hierchyElemnetIdsToChange[] = 69;  //  Домашняя коллекция -> Полотенца
+    $hierchyElemnetIdsToChange[] = 70;  //  Домашняя коллекция -> Постельное белье
+    
+    $hierchyElemnetIdsToChange[] = 79;  //  Коляски и аксессуары -> Вкладыши
+    $hierchyElemnetIdsToChange[] = 80;  //  Коляски и аксессуары -> Дождевики
+    $hierchyElemnetIdsToChange[] = 81;  //  Коляски и аксессуары -> Коляски
+    $hierchyElemnetIdsToChange[] = 82;  //  Коляски и аксессуары -> Конверты
+    $hierchyElemnetIdsToChange[] = 83;  //  Коляски и аксессуары -> Москитные сетки
     
     foreach ($hierchyElemnetIdsToChange as $hierarchyElementId) {
         $page = $hierarchy->getElement($hierarchyElementId);
         if($page instanceof umiHierarchyElement){
             echo $page->name.' - '.$page->getObjectTypeId()."<br>";
+            if($page->getObjectTypeId() == $objectTypeId){
+                $object = $page->getObject();
+                $object->setTypeId($categoryTypeId);
+                $object->commit();
+            }
         }
     }
+    
+    echo 'ok';
     
 ?>
