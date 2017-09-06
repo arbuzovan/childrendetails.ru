f03e3560de5d8fea4f78d93d24f80ecc4bed0fc4
arbuzovan <arbuzovan@yandex.ru>
2017-09-06 13:21:30 +0300

    добавлено под наблюдение

---
+++ classes/components/news/permissions.php
@@ -0,0 +1,35 @@
+<?php
+	/**
+	 * Группы прав на функционал модуля
+	 */
+	$permissions = [
+		/**
+		 * Права на просмотр новостей
+		 */
+		'view' => [
+			'lastlist',
+			'listlents',
+			'rubric',
+			'related_links',
+			'rss',
+			'item',
+			'lastlents'
+		],
+		/**
+		 * Права на администрирование модуля
+		 */
+		'lists' => [
+			'lists',
+			'subjects',
+			'add',
+			'edit',
+			'del',
+			'activity',
+			'rss_list',
+			'config',
+			'item.edit',
+			'rubric.edit',
+			'publish'
+		]
+	];
+?>
